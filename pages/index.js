ce0a6747488f8e8855ce95bf5100f62263f82995
yogarp <yogarizky51@gmail.com>
2023-10-09 10:25:25 +0700

    Tugas Hari-6

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,10 @@ import Layout from "@/layouts";
 export default function Home() {
   return (
     <>
-      <Layout>
+      <Layout
+        metaTitle={"Home"}
+        metaDescription={"Ini adalah Halaman Home Atau Landing Page"}
+      >
         <p className="bg-fuchsia-500">Home</p>
       </Layout>
     </>
--- pages/profile.js
+++ pages/profile.js
@@ -3,7 +3,10 @@ import Layout from "@/layouts";
 export default function Profile() {
   return (
     <>
-      <Layout>
+      <Layout
+        metaTitle={"Profile"}
+        metaDescription={"Ini adalah Halaman Profile"}
+      >
         <p className="bg-orange-500">Profile</p>
       </Layout>
     </>
