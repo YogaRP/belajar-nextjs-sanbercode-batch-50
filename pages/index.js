b3f940cc3a0373579828002c9efdf044ef674cd4
yogarp <yogarizky51@gmail.com>
2023-10-13 06:54:14 +0700

    Tugas Hari-9

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,13 @@
 import Layout from "@/layouts";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    fetch("/api/hello")
+      .then((res) => res.json())
+      .then((res) => console.log("response =>", res))
+      .catch((err) => console.log(err));
+  }, []);
   return (
     <>
       <Layout
